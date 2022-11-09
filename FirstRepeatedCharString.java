239e7e4341dfbe3dc4fded037ef7a2a0102b4f1c
YashodharH <115421544+YashodharH@users.noreply.github.com>
2022-11-09 08:28:25 +0530

    Add files via upload

--- FirstRepeatedCharString.java
+++ FirstRepeatedCharString.java
@@ -4,9 +4,27 @@ public class FirstRepeatedCharString {
 
 	public static void main(String[] args) {
 		// First Repeated character in a string
-		
-		
 
+		String name = "Yashodhar";
+		char [] a = name.toCharArray();
+		boolean [] visited = new boolean [a.length];
+
+		for(int i=0; i<a.length; i++) {
+			if(visited[i] == true) {
+				continue;
+			}
+			int count = 0;
+			for(int j=0; j<a.length; j++) {
+				if(a[i] == a[j]) {
+					count++;
+					visited[j] = true;
+				}
+			}
+			if (count>1) {
+				System.out.println(a[i]);
+				break;
+			}
+		}
 	}
 
 }
